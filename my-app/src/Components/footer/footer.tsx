27b5bc365a3cfb67bd9a6fc21d98881da9e2e8b1
Sona <sonalavrushina@gmail.com>
2023-07-24 01:47:48 +0300

    fix: pagination bug

--- my-app/src/Components/footer/footer.tsx
+++ my-app/src/Components/footer/footer.tsx
@@ -1,41 +1,37 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { SET_CURRENT_DATA } from "../../redux/data";
+import "./footer.css";
+import { useDispatch, useSelector } from "react-redux";
 import { initialStateInterface } from "../../redux/initialState";
 import { MAX_PAGE_ELEMENTS, ONE_PAGE } from "../../utils/const";
-import "./footer.css";
+import { NEXT_PAGE, PREV_PAGE } from "../../redux/page";
+import { AnyAction } from "@reduxjs/toolkit";
 
 function Footer() {
+  const currentPage = useSelector(
+    (item: initialStateInterface) => item.pages.page
+  );
   const data = useSelector(
-    (item: initialStateInterface) => item.data.cellData
+    (item: initialStateInterface) => item.data.currentCellData
   );
   const dispatch = useDispatch();
   const totalPages = Math.ceil(data.length / MAX_PAGE_ELEMENTS);
   const currentPageNum = [
     ...Array(Math.min(MAX_PAGE_ELEMENTS, totalPages)),
   ];
-  const [currentPage, setCurrentPage] = useState(ONE_PAGE);
 
-  const handlePageChange = (newPage: number) => {
-    if (newPage >= ONE_PAGE && newPage <= totalPages) {
-      setCurrentPage(newPage);
-      const firstIndex = (newPage - ONE_PAGE) * MAX_PAGE_ELEMENTS;
-      const lastIndex = firstIndex + MAX_PAGE_ELEMENTS;
-      const currentPageData = data.slice(firstIndex, lastIndex);
-      dispatch(SET_CURRENT_DATA(currentPageData));
-    }
-  };
+  function pagination(action: () => AnyAction) {
+    dispatch(action());
+  }
 
   return (
     <div className={"footer_wrapper"}>
-      <Link to={`./${currentPage - ONE_PAGE}`}>
-        <button
-          onClick={() => handlePageChange(currentPage - ONE_PAGE)}
-        >
-          Назад
-        </button>
-      </Link>
+      {currentPage === ONE_PAGE ? (
+        <button>Назад</button>
+      ) : (
+        <Link to={`./${currentPage - ONE_PAGE}`}>
+          <button onClick={() => pagination(PREV_PAGE)}>Назад</button>
+        </Link>
+      )}
       <span className={"pagination_block"}>
         {currentPageNum.map((_, index) => {
           const pageNumber = index + ONE_PAGE;
@@ -53,11 +49,13 @@ function Footer() {
           );
         })}
       </span>
-      <Link to={`./${currentPage + 1}`}>
-        <button onClick={() => handlePageChange(currentPage + 1)}>
-          Далее
-        </button>
-      </Link>
+      {currentPage === MAX_PAGE_ELEMENTS ? (
+        <button>Далее</button>
+      ) : (
+        <Link to={`./${currentPage + 1}`}>
+          <button onClick={() => pagination(NEXT_PAGE)}>Далее</button>
+        </Link>
+      )}
     </div>
   );
 }
